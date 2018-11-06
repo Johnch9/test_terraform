3f95fa098f551b91fedbe2a3dcc2b9be567d3c38
Howard <P95048490@capita.co.uk>
2018-11-06 14:51:07 +0000

    removed helm

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 #Setup Helm provider
-provider "helm" {
-  kubernetes {
-        # host     = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
-        # username = "${azurerm_kubernetes_cluster.k8s.kube_config.0.username}"
-        # password = "${azurerm_kubernetes_cluster.k8s.kube_config.0.password}"
+# provider "helm" {
+#   kubernetes {
+#         # host     = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
+#         # username = "${azurerm_kubernetes_cluster.k8s.kube_config.0.username}"
+#         # password = "${azurerm_kubernetes_cluster.k8s.kube_config.0.password}"
 
-        # client_certificate     = "${azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate}"
-        # client_key             = "${azurerm_kubernetes_cluster.k8s.kube_config.0.client_key}"
-        # cluster_ca_certificate = "${azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate}"
-#    config_path = "${azurerm_kubernetes_cluster.k8s.kube_config_raw}"
-    config_path = "./azurek8s"
-  }
-}
+#         # client_certificate     = "${azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate}"
+#         # client_key             = "${azurerm_kubernetes_cluster.k8s.kube_config.0.client_key}"
+#         # cluster_ca_certificate = "${azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate}"
+# #    config_path = "${azurerm_kubernetes_cluster.k8s.kube_config_raw}"
+#     config_path = "./azurek8s"
+#   }
+# }
 
 resource "null_resource" "first" {
     provisioner "local-exec" {
@@ -34,23 +34,23 @@ resource "null_resource" "third" {
 }
 
 #Add Helm Repo for SVC Cat
-resource "helm_repository" "incubator" {
-  name = "incubator"
-#  url  = "https://kubernetes-charts-incubator.storage.googleapis.com"
-  url  = "./istio/install/kubernetes/helm/"
-}
+# resource "helm_repository" "incubator" {
+#   name = "incubator"
+# #  url  = "https://kubernetes-charts-incubator.storage.googleapis.com"
+#   url  = "./istio/install/kubernetes/helm/"
+# }
 
 #Deploy SvcCat
-resource "helm_release" "istio" {
-  name       = "istio"
-#  repository = "${helm_repository.incubator.metadata.0.name}"
-  repository = "./"
-#  repository = "./istio/install/kubernetes/helm/"
-  chart      = "istio"
-  namespace  = "istio"
+# resource "helm_release" "istio" {
+#   name       = "istio"
+# #  repository = "${helm_repository.incubator.metadata.0.name}"
+#   repository = "./"
+# #  repository = "./istio/install/kubernetes/helm/"
+#   chart      = "istio"
+#   namespace  = "istio"
 
-  set {
-    name  = "istio.sidecar-injector"
-    value = true
-  }
-}
+#   set {
+#     name  = "istio.sidecar-injector"
+#     value = true
+#   }
+# }
