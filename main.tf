a681791bdf72db92a9f654e236afc6a741d8f52f
Howard <P95048490@capita.co.uk>
2018-11-06 10:00:45 +0000

    Added null_resource

--- main.tf
+++ main.tf
@@ -6,23 +6,43 @@ provider "helm" {
   }
 }
 
-#Add Helm Repo for SVC Cat
-resource "helm_repository" "incubator" {
-  name = "incubator"
-#  url  = "https://kubernetes-charts-incubator.storage.googleapis.com"
-  url  = "./istio/install/kubernetes/helm/"
+resource "null_resource" "first" {
+    provisioner "local-exec" {
+        command = "echo 'first'"
+    }
 }
 
-#Deploy SvcCat
-resource "helm_release" "istio" {
-  name       = "istio"
-#  repository = "${helm_repository.incubator.metadata.0.name}"
-  repository = "./istio/install/kubernetes/helm/"
-  chart      = "istio"
-  namespace  = "istio"
+resource "null_resource" "second" {
+    depends_on = ["null_resource.first"]
+    provisioner "local-exec" {
+        command = "echo 'second'"
+    }
+}
 
-  set {
-    name  = "istio.sidecar-injector"
-    value = true
-  }
+resource "null_resource" "third" {
+    depends_on = ["null_resource.second"]
+    provisioner "local-exec" {
+        command = "echo 'third'"
+    }
+}
+
+#Add Helm Repo for SVC Cat
+# resource "helm_repository" "incubator" {
+#   name = "incubator"
+# #  url  = "https://kubernetes-charts-incubator.storage.googleapis.com"
+#   url  = "./istio/install/kubernetes/helm/"
+# }
+
+#Deploy SvcCat
+# resource "helm_release" "istio" {
+#   name       = "istio"
+# #  repository = "${helm_repository.incubator.metadata.0.name}"
+#   repository = "./istio/install/kubernetes/helm/"
+#   chart      = "istio"
+#   namespace  = "istio"
+
+#   set {
+#     name  = "istio.sidecar-injector"
+#     value = true
+#   }
 }
